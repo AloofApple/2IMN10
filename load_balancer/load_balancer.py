5406282ebc20bbc8f3cf707e877874ee6bf75f56
Jay <t.chou@student.tue.nl>
2025-10-01 21:26:54 +0200

    load balancer

--- load_balancer/load_balancer.py
+++ load_balancer/load_balancer.py
@@ -20,16 +20,17 @@ class LoadBalancer:
     def __init__(self, servers):
         self.servers = servers
         self.connections = {server: 0 for server in servers}
+        self.healthy = {server: True for server in servers}
         self.index = 0
-        self.lock = asyncio.Lock()
+        self.conn_lock = asyncio.Lock()
 
     # Methods to update connection counts
     def increment_connection(self, server):
-        self.connections[server] += 1
+       self.connections[server] += 1
 
     async def decrement_connection(self, server):
-        async with self.lock:
-                self.connections[server] -= 1
+        async with self.conn_lock:
+            self.connections[server] -= 1
 
     # Static approaches
     def round_robin(self, servers):
@@ -43,25 +44,12 @@ class LoadBalancer:
     
     # Dynamic approach
     def least_connections(self, servers):
-        # filter the active server counts based on the servers given.
-        connections = {server: self.connections.get(server) for server in servers}
-        least_connected = None
-        min_connections = float('inf')
-
-        # find the server with the least active connections.
-        for server, count in connections.items():
-            if count < min_connections:
-                min_connections = count
-                least_connected = server
-
-        return least_connected
-
+        return min(servers, key=lambda s: self.connections.get(s, 0))
+    
     # The method to get the server based on the chosen algorithm
     async def get_server(self):
-        # You can switch between different algorithms here
-        async with self.lock: 
-            server = self.round_robin(self.servers)
-
+        async with self.conn_lock:
+            server = self.least_connections(self.servers)
             self.increment_connection(server)
 
-            return server
+        return server
