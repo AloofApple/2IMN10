8644fbf3b6cd204215517571219b1fcdf054205b
Jay <t.chou@student.tue.nl>
2025-10-01 21:28:26 +0200

    load balancer

--- load_balancer/load_balancer.py
+++ load_balancer/load_balancer.py
@@ -22,7 +22,9 @@ class LoadBalancer:
         self.connections = {server: 0 for server in servers}
         self.healthy = {server: True for server in servers}
         self.index = 0
+        self.check_interval = 3  # seconds
         self.conn_lock = asyncio.Lock()
+        self.health_lock = asyncio.Lock()
 
     # Methods to update connection counts
     def increment_connection(self, server):
@@ -46,10 +48,48 @@ class LoadBalancer:
     def least_connections(self, servers):
         return min(servers, key=lambda s: self.connections.get(s, 0))
     
+    # Set a server healthy or unhealthy with a lock
+    async def set_health(self, server, status: bool):
+        async with self.health_lock:
+            self.healthy[server] = status
+
+    # Check if the server is healthy or not
+    async def is_healthy(self, server):
+        async with self.health_lock:
+            return self.healthy.get(server, False)
+    
+    # Periodic health check
+    async def health_check(self):
+        while True:
+            for host, port in self.servers:
+                try:
+                    # Check the server health and set it to True
+                    reader, writer = await asyncio.open_connection(host, port)
+                    await self.set_health((host, port), True)
+                    logging.info(f"{host}:{port} is HEALTHY 😀")
+                    
+                    # Wait until writer is fully closed.
+                    writer.close()
+                    await writer.wait_closed()
+
+                except Exception:
+                    # Set the server health to False
+                    await self.set_health((host, port), False)
+                    logging.warning(f"{host}:{port} is UNHEALTHY 😵")
+                    
+            await asyncio.sleep(self.check_interval)
+
     # The method to get the server based on the chosen algorithm
     async def get_server(self):
+        async with self.health_lock:
+            healthy_servers = [s for s in self.servers if self.healthy.get(s, False)]
+
+        if not healthy_servers:
+            logging.error("No healthy servers available!")
+            return None
+
         async with self.conn_lock:
-            server = self.least_connections(self.servers)
+            server = self.least_connections(healthy_servers)
             self.increment_connection(server)
 
         return server
